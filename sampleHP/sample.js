558561360f61a2a542b7b72ca7ffaadd28930c2b
aki29iwasawa <skk291838@gmail.com>
2020-08-19 15:35:02 +0900

    [update] sampleHP

--- sampleHP/sample.js
+++ sampleHP/sample.js
@@ -12,22 +12,49 @@ article.appendChild(img);
 
 
 /*ーーーーーーーーdiv <h1>と<form>を入れるーーーーーー */
-/*ーーdivの作成ーー */
+/*ーーーーdivの作成ーーーー */
 
 var div = document.createElement("div");
 article.appendChild(div);
 
-/*ーー<h1>の作成ーー */
+/*ーーーー<h1>の作成ーーーー */
 
 div.innerHTML = "<h1>Sign In<h1>";
 
-/*ーー<form>の作成ーー */
+/*ーーーー<form>の作成ーーーー */
 
 var form = document.createElement("form");
+form.name = "form";
+/*form.onsubmit="return check"*/
+
 div.appendChild(form);
 
+/*ーメールアドレスー*/
+
+form.innerHTML = "<p><label for='mail'>アカウントID</label></p>";
+
 var input = document.createElement("input");
+input.classList.add("box");
+input.type = "email";
+input.name = "mail"
+input.id = "mail";
+input.placeholder = "Email";
+
 form.appendChild(input);
+
+
+/*ーパスワードー*/
+/*
+form.innerHTML = "<p><label for='password'>パスワード</label></p>";
+
+var input2 = document.createElement("input");
+input2.classList.add("box");
+input2.type = "password";
+input2.name = "password";
+input2.id = "password";
+input2.placeholder = "●●●●●●";
+
+form.appendChild(input2);
 
 
 
